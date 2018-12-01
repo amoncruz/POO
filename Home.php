26afab0fb77ebad5a1c5c1edaaf4d5820a95d719
amon cruz <amoncruz13@gmail.com>
2018-12-01 17:29:27 -0300

    ma

--- Home.php
+++ Home.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  
-    <link rel="stylesheet" href="style.css">
+   
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Noto Sans TC' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
     <title>Desvendando Direito 2</title>
 </head>
 <body>
@@ -16,38 +16,36 @@
   
 <nav class="navbar navbar-expand-sm navbar-light bg-white">
   <div class="container">
-  <a class="navbar-brand" href="#"><img  src="lady.jpg" style="height:75px;width:350px;"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+  <a class="navbar-brand" href="#"><img class="d-none d-lg-block img mr-3" src="lady.jpg" style="height:70px;width:350px;"></a>
+  <div class="container">
   <div class="col-12">
-    <ul class="navbar-nav justify-content-start mb-2 mr-4">
+    <ul class="navbar-nav justify-content-center mb-2 mr-4">
       <li class="nav-item active">
-        <a class="nav-link p-1" data-toggle="" href="#">SIGNIFICADOS<span class="d-none d-sm-inline-block pl-2">&nbsp| </span><span class="sr-only">(current)</span></a>
+        <a class=" p-2 h5" data-toggle="" href="#">SIGNIFICADOS<span class="d-none d-sm-inline-block pl-2">&nbsp| </span><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active"> 
-        <a class="nav-link p-1" href="#">PUBLICAÇÕES<span class="d-none d-sm-inline-block pl-2" >&nbsp| </span></a>
+        <a class=" p-2 h5" href="#">PUBLICAÇÕES<span class="d-none d-sm-inline-block pl-2" >&nbsp| </span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link p-1" href="#">QUEM SOMOS </a>
+        <a class="p-2 h5" href="#">QUEM SOMOS </a>
       </li>
 
     </ul>
-   <div class="d-none  d-md-block">
+   <div class="d-none d-md-block">
     <form class="form-group row my-2 my-lg-0 justify-content-start ml-2">
-  <div class=" row col-md-6  col-lg-12 p-0">
-      <input class="form-control col-lg-10" type="search" placeholder="O que vc esta procurando" aria-label="Search">
+  <div class=" row col-md-12  col-lg-12 p-0 justify-content-center">
+      <input class="form-control col-md-10 col-lg-11" type="search" placeholder="O que você esta procurando?" aria-label="Search">
       <button class="btn btn-sm " type="submit" style="background-color:#FF7C00;height:38px;"><img src="icons/lupa2.svg"></button>
       </div>
       </div>
   
     
     </form>
-   </div>
-    </div>
   </div>
-</div>
+   </div>
+   
+
+
 </nav>
 
 
@@ -61,40 +59,55 @@
 <div class="mt-1"><a href="#">&nbspINÍCIO</a></div>
 </div>
 <div class="container2">
-<h4>SIGNIFICADOS</h4>
+<h4 class="font-weight-bold">SIGNIFICADOS</h4>
 <hr>
 <div class="row">
-    <div class="col-sm ">
+    <div class="col-sm mt-4">
       <h4>Familia</h4>
-Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fugiat et occaecat ex qui. Laborum cillum tempor et qui laboris reprehenderit id tempor laborum sint labore excepteur. Consectetur adipisicing occaecat irure amet incididunt labore. Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+      Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
     </div>
-    <div class="col-sm">
+    <div class="col-sm mt-4">
      <h4> Trabalho</h4>
-     Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fugiat et occaecat ex qui. Laborum cillum tempor et qui laboris reprehenderit id tempor laborum sint labore excepteur. Consectetur adipisicing occaecat irure amet incididunt labore. Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+     Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
 
     </div>
-    <div class="col-sm">
+    <div class="col-sm mt-4">
      <h4> Seguridade</h4>
-     Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fugiat et occaecat ex qui. Laborum cillum tempor et qui laboris reprehenderit id tempor laborum sint labore excepteur. Consectetur adipisicing occaecat irure amet incididunt labore. Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+     Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
 
     </div>
   </div>
-  <div class="row mt-4 ">
-  <div class="col-sm ">
+  <div class="row">
+  <div class="col-sm mt-4">
       <h4>Sucessões</h4>
-Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fugiat et occaecat ex qui. Laborum cillum tempor et qui laboris reprehenderit id tempor laborum sint labore excepteur. Consectetur adipisicing occaecat irure amet incididunt labore. Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+      Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
     </div>
-    <div class="col-sm">
+    <div class="col-sm mt-4">
      <h4> Imobiliario</h4>
-     Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fugiat et occaecat ex qui. Laborum cillum tempor et qui laboris reprehenderit id tempor laborum sint labore excepteur. Consectetur adipisicing occaecat irure amet incididunt labore. Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+     Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
 
     </div>
-    <div class="col-sm">
+    <div class="col-sm mt-4">
      <h4>Comercial</h4>
-     Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fugiat et occaecat ex qui. Laborum cillum tempor et qui laboris reprehenderit id tempor laborum sint labore excepteur. Consectetur adipisicing occaecat irure amet incididunt labore. Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
-      </div>
+     Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+    </div>
    
-
+    </div>
+    <div class="row">
+            <div class="col-sm mt-4">
+                <h4>Consumidor</h4>
+                Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+            </div>
+              <div class="col-sm mt-4">
+               <h4>Atividade Jurica</h4>
+               Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+            </div>
+              <div class="col-sm mt-4">
+               <h4>Civil        </h4>
+                Esse Lorem laborum exercitation aliquip reprehenderit irure Lorem reprehenderit officia culpa aliqua dolore.
+              </div>
+             
+              </div>
 
 </section>
 
@@ -104,27 +117,39 @@ Culpa tempor nulla mollit nostrud mollit amet sit culpa labore. Dolor nostrud fu
 <div class="espacamento">
 
 </div>
-<h4>BLOG</h4>
+<h4 class="font-weight-bold">BLOG</h4>
 <hr>
 
-<div class="media p-0"  style="-webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
--moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);">
-  <img class="mr-3 d-none d-md-block" src="lady.jpg"  style="height:130px;width:130px;" alt="Generic placeholder image">
+<div class="media p-3 p-lg-1">
+  <img class="mr-3 d-none d-lg-block " src="lady.jpg"  style="height:130px;width:130px;" alt="Generic placeholder image">
   <div class="media-body">
-    <h5 class="mt-0">Titulo</h5>
-    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-   <br><a href="#" class="text-dark ml-3 "><img src="icons/share.svg">Compartilhar</a>
+    <h5 class="m-0">Titulo</h5>
+    Cras sit amet nibh libero, in gravidanulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+   <br><a href="#" class="text-dark ml-0 text-muted "><img src="icons/share.svg">Compartilhar</a>
   </div>
 </div>
-<div class="media mt-3"  style="-webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
--moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);">
-  <img class="mr-3 d-none d-md-block" src="lady.jpg"  style="height:130px;width:130px;" alt="Generic placeholder image">
+<div class="media mt-3 p-3 p-lg-1">
+        <img class="mr-3 d-none d-lg-block" src="lady.jpg"  style="height:130px;width:130px;" alt="Generic placeholder image">
+        <div class="media-body">
+          <h5 class="m-0">Titulo</h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+         <br><a href="#" class="text-dark ml-0 text-muted "><img src="icons/share.svg">Compartilhar</a>
+        </div>
+      </div>
+      <div class="media mt-3 p-3 p-lg-1">
+            <img class="mr-3 d-none d-lg-block" src="lady.jpg"  style="height:130px;width:130px;" alt="Generic placeholder image">
+            <div class="media-body">
+              <h5 class="m-0">Titulo</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+             <br><a href="#" class="text-dark ml-0 text-muted "><img src="icons/share.svg">Compartilhar</a>
+            </div>
+          </div>
+<div class="media mt-3 p-3 p-lg-1">
+  <img class="mr-3 d-none d-lg-block" src="lady.jpg"  style="height:130px;width:130px;" alt="Generic placeholder image">
   <div class="media-body">
-    <h5 class="mt-0">Titulo</h5>
+    <h5 class="m-0">Titulo</h5>
     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-    <br><a href="#" class="text-dark ml-3 "><img src="icons/share.svg">Compartilhar</a>
+    <br><a href="#" class="text-dark ml-0 text-muted"><img src="icons/share.svg">Compartilhar</a>
   </div>
 </div>
 
@@ -133,7 +158,11 @@ box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);">
 </div>
 </div>
 <footer class="text-center">
-<p>Eajwdkajdakjdakdjawkdjakdj</p>
+    <div class="espacamento mt-2" style="background-color:#c4c4c4;"></div>
+<p>admin@desvendandodireito.com.br</p>
+<hr>
+<p>COPYRIGHT DESVENDANDO DIREITO 2018</p>
+<p>BY MyPROJECT</p>
 </footer>
 
 
